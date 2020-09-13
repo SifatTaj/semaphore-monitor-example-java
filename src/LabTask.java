41c2b0f9bf85f944c2bc4998985c8146500f2f00
sifattaj <github047>
2020-09-13 12:54:31 +0600

    Lab task added

--- src/LabTask.java
+++ src/LabTask.java
@@ -3,11 +3,14 @@
  * The task is similar to the producer-consumer problem discussed in the video.
  * There is a SharedMemory class used by two threads. The WriterThread writes
  * some string to the shared memory at a random interval. The ReaderThread reads
- * the memory and prints those string.
+ * the memory and prints those strings.
  *
  * If the memory is empty, the ReaderThread
  * goes to the suspended state. Whenever a new item is inserted, the WriterThread
  * give the suspended ReaderThread a wakeup call.
+ *
+ * You only need to modify the SharedMemory class.
+ * Best of luck
  *
  * @author Sifat Ut Taki
  * BRAC University
@@ -21,21 +24,27 @@ class SharedMemory {
 
   private List<String> registers = new ArrayList<>();
 
-  public String readFromReg() throws InterruptedException {
-    // Use monitor for this method (Java Synchronization)
-    // if pointer is at -1 that means no item in the list. So, suspend readerThread (using wait())
-    // take the value from the the list,
-    // decrease pointer,
-    // remove the value from the list,
-    // return the value
+  public String readFromReg() {
+    /**
+     * TODO:
+     * 1. Use monitor for this method [public synchronized String readFromReg()].
+     * 2. If the pointer is at -1, it means there is no item in the list. So, suspend the readerThread [using wait()].
+     * 3. Take the value from the the list.
+     * 4. Decrease the pointer by 1.
+     * 5. Remove the string from the list,
+     * 6. Return the value
+     */
     return "";
   }
 
   public void writeToReg(String value) {
-    // Use monitor for this method (Java Synchronization)
-    // Add the value to the list
-    // increase pointer
-    // if pointer is at 0 that means first item in the list. So, wakeup readerThread (using notifyAll())
+    /**
+     * TODO:
+     * 1. Use monitor for this method [public synchronized String writeToReg()].
+     * 2. Add the string to the list.
+     * 3. Increase the pointer by 1.
+     * 4. If the pointer is at 0, it indicates first item in the list. So, wakeup readerThread [using notifyAll()].
+     */
   }
 }
 
@@ -86,7 +95,7 @@ class ReaderThread extends Thread {
   public void run() {
     try {
       while (true) {
-        System.out.println(sharedMemory.readFromReg());
+        System.out.println(Thread.currentThread().getName() + " prints: " + sharedMemory.readFromReg());
       }
     } catch (InterruptedException e) {
       e.printStackTrace();
